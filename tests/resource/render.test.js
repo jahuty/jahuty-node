5c03f875e00948cb0b6ec80880c4e3c13d38ff56
Jack Clayton <clayjs0@gmail.com>
2021-02-27 08:28:43 -0700

    Add from method back to resources

--- tests/resource/render.test.js
+++ tests/resource/render.test.js
@@ -11,6 +11,30 @@ describe('Snippet', () => {
     });
   });
 
+  describe('.from', () => {
+    let params;
+
+    beforeEach(() => {
+      params = { content: 'foo' };
+    });
+
+    it('rasies error when content is missing', () => {
+      delete params.content;
+
+      expect(() => Render.from(params)).toThrow(Error);
+    });
+
+    it('does not rasie error when extra attribute is present', () => {
+      params.foo = 'bar';
+
+      expect(() => Render.from(params)).not.toThrow(Error);
+    });
+
+    it('returns snippet', () => {
+      expect(Render.from(params)).toBeInstanceOf(Render);
+    });
+  });
+
   describe('.toString()', () => {
     it('returns content', () => {
       expect(render.toString()).toEqual(content);
