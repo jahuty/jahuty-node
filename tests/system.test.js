c66d762603e2bc22651df9cd74c1b21de6648d19
Jack Clayton <clayjs0@gmail.com>
2021-04-30 08:21:28 -0600

    Add - and group - system tests

--- tests/system.test.js
+++ tests/system.test.js
@@ -7,74 +7,96 @@ describe('System', () => {
   const render1 = { snippetId: 1, content: '<p>This is my first snippet!</p>' };
   const render62 = { snippetId: 62, content: '<p>This foo is bar.</p>' };
 
-  describe('when the user renders a static snippet', () => {
-    it('renders the snippet', async () => {
-      let render = await jahuty.snippets.render(1);
+  const publishedRender102 = { snippetId: 102, content: '<p>This content is published.</p>' }
+  const latestRender102 = { snippetId: 102, content: '<p>This content is latest.</p>' }
 
-      expect(render.content).toBe(render1.content);
+  describe('user renders one snippet' , () => {
+    describe('with a static snippet', () => {
+      it('renders the snippet', async () => {
+        let render = await jahuty.snippets.render(1);
 
-      // A second render should use the cached value.
-      const start = new Date().getTime();
-      render = await jahuty.snippets.render(1);
-      const end = new Date().getTime();
+        expect(render.content).toBe(render1.content);
 
-      expect(end - start).toBeLessThan(5);
-      // Verify the content is correct (see #26 for details).
-      expect(render.content).toBe(render1.content);
-    });
-  });
+        // A second render should use the cached value.
+        const start = new Date().getTime();
+        render = await jahuty.snippets.render(1);
+        const end = new Date().getTime();
 
-  describe('when the user renders a parameterized snippet', () => {
-    it('renders the snippet', async () => {
-      const params = { foo: 'foo', bar: 'bar' };
-
-      const render = await jahuty.snippets.render(62, { params });
-
-      expect(render.content).toBe(render62.content);
-
-      // A second render with the same params should use the cached value.
-      let start = new Date();
-      await jahuty.snippets.render(62, { params });
-      let end = new Date();
-
-      expect(end.getTime() - start.getTime()).toBeLessThan(5);
-
-      // A third render with different params should render the snippet.
-      params.bar = 'baz';
-
-      start = new Date();
-      await jahuty.snippets.render(62, { params });
-      end = new Date();
-
-      expect(end.getTime() - start.getTime()).toBeGreaterThan(5);
-    });
-  });
-
-  describe('when the user renders many snippets', () => {
-    it('renders the snippets', async () => {
-      // This request should return two renders.
-      const renders = await jahuty.snippets.allRenders('test', {
-        params: { '*': { foo: 'foo' }, 62: { bar: 'bar' } },
+        expect(end - start).toBeLessThan(5);
+        // Verify the content is correct (see #26 for details).
+        expect(render.content).toBe(render1.content);
       });
+    });
 
-      expect(renders).toHaveLength(2);
-      expect(renders).toContainEqual(render1);
-      expect(renders).toContainEqual(render62);
+    describe('with parameters', () => {
+      it('renders the snippet', async () => {
+        const params = { foo: 'foo', bar: 'bar' };
 
-      // A call to render a snippet in the collection should use the cached value.
-      let start = new Date();
-      await jahuty.snippets.render(1);
-      let end = new Date();
+        const render = await jahuty.snippets.render(62, { params });
 
-      expect(end.getTime() - start.getTime()).toBeLessThan(5);
+        expect(render.content).toBe(render62.content);
 
-      // A call to render a snippet in the collection with the equivalent params
-      // should use the cached value.
-      start = new Date();
-      await jahuty.snippets.render(62, { params: { foo: 'foo', bar: 'bar' } });
-      end = new Date();
+        // A second render with the same params should use the cached value.
+        let start = new Date();
+        await jahuty.snippets.render(62, { params });
+        let end = new Date();
 
-      expect(end.getTime() - start.getTime()).toBeLessThan(5);
+        expect(end.getTime() - start.getTime()).toBeLessThan(5);
+
+        // A third render with different params should render the snippet.
+        params.bar = 'baz';
+
+        start = new Date();
+        await jahuty.snippets.render(62, { params });
+        end = new Date();
+
+        expect(end.getTime() - start.getTime()).toBeGreaterThan(5);
+      });
+    });
+
+    describe('with latest content', () => {
+      it('renders the snippets', async () => {
+        const render = await jahuty.snippets.render(102, { preferLatest: true });
+
+        expect(render).toEqual(latestRender102);
+      });
+    });
+  });
+
+  describe('user renders many snippets', () => {
+    describe('with parameters', () => {
+      it('renders the snippets', async () => {
+        const renders = await jahuty.snippets.allRenders('test', {
+          params: { '*': { foo: 'foo' }, 62: { bar: 'bar' } },
+        });
+
+        expect(renders).toContainEqual(render1);
+        expect(renders).toContainEqual(render62);
+        expect(renders).toContainEqual(publishedRender102);
+
+        // A call to render a snippet in the collection should use the cached value.
+        let start = new Date();
+        await jahuty.snippets.render(1);
+        let end = new Date();
+
+        expect(end.getTime() - start.getTime()).toBeLessThan(5);
+
+        // A call to render a snippet in the collection with the equivalent params
+        // should use the cached value.
+        start = new Date();
+        await jahuty.snippets.render(62, { params: { foo: 'foo', bar: 'bar' } });
+        end = new Date();
+
+        expect(end.getTime() - start.getTime()).toBeLessThan(5);
+      });
+    });
+
+    describe('with latest content', () => {
+      it('renders the snippets', async () => {
+        const renders = await jahuty.snippets.allRenders('test', { preferLatest: true });
+
+        expect(renders).toContainEqual(latestRender102);
+      });
     });
   });
 });
